56205185dd61575e7835bc23a6ca5dadc6ca02ca
HongGuShi <562988147@qq.com>
2019-04-20 19:29:00 +0800

    切面基础配置更新

--- panghu-common/src/main/java/com/panghu/common/aspect/LogAspectController.java
+++ panghu-common/src/main/java/com/panghu/common/aspect/LogAspectController.java
@@ -6,7 +6,6 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
